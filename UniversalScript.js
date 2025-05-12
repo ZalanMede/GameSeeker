e289c7c1bf5ab2dede4cbad3dbb8d46e33d4b923
ZalanMede <medezalan@turr.hu>
2025-05-12 12:52:04 +0200

    GAM-71 SM Menü egy oldalon

--- UniversalScript.js
+++ UniversalScript.js
@@ -15,19 +15,37 @@ function scrollToTop() {
 }
 
 
-function OpenCloseMDMenu() {
+function OpenCloseMDSMMenu() {
   if (openMD)
   {
-    document.getElementById("overlayMD").style.height = "0%";
-    document.getElementById("opencloseOverlaySVG");
-    opencloseOverlaySVG.style.animation = "quarterSpinBack 0.5s linear 1 forwards";
+    if (!(document.documentElement.clientWidth < 576))
+    {
+      document.getElementById("overlayMD").style.height = "0%";
+      document.getElementById("opencloseOverlaySVG");
+      opencloseOverlaySVG.style.animation = "quarterSpinBack 0.5s linear 1 forwards";
+    }
+    else
+    {
+      document.getElementById("overlaySM").style.height = "0%";
+      document.getElementById("opencloseOverlaySVG_SM");
+      opencloseOverlaySVG_SM.style.animation = "halfSpinBack 0.5s linear 1 forwards";
+    }
     openMD = false;
   }
   else
   {
-    document.getElementById("overlayMD").style.height = "100%";
-    document.getElementById("opencloseOverlaySVG");
-    opencloseOverlaySVG.style.animation = "quarterSpinForw 0.5s linear 1 forwards";
+    if (!(document.documentElement.clientWidth < 576))
+    {
+      document.getElementById("overlayMD").style.height = "100%";
+      document.getElementById("opencloseOverlaySVG");
+      opencloseOverlaySVG.style.animation = "quarterSpinForw 0.5s linear 1 forwards";
+    }
+    else
+    {
+      document.getElementById("overlaySM").style.height = "100%";
+      document.getElementById("opencloseOverlaySVG_SM");
+      opencloseOverlaySVG_SM.style.animation = "halfSpinForw 0.5s linear 1 forwards";
+    }
     openMD = true;
   }
 }
